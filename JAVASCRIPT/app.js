83e884bd912442c9624e304a71bb5c89e7e1401b
KIBET G👨‍💻👨‍💻 <kibetg984@gmail.com>
2024-10-19 09:55:28 +0300

    We made changes on the Javascript file so that we can know if it fetches any form of data from our API Key from news.org

---
+++ JAVASCRIPT/app.js
@@ -0,0 +1,7 @@
+//We have declared our API Key for the project to fetch data for the news
+const API_KEY = '';
+const API_URL = '';
+
+document.addEventListener('DOMContentLoaded',()=>{
+    fetchNews('general');
+});
